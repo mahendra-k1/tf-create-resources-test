6a2614d2f0e47da71da28c2ef98011401a8415ab
mahendra-k1 <mahendra4int@gmail.com>
2025-08-09 16:42:20 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.111.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
+}
+
+output "resource_group_location" {
+  value = azurerm_resource_group.example.location
+}
